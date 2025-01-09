d0a1939d09e635ba60c2353541197f7f6d26b753
ahammad <ahammad@student.42.fr>
2025-01-09 14:49:43 +0100

    use assert

--- ex04/whatis.py
+++ ex04/whatis.py
@@ -1,4 +1,4 @@
-import sys
+"""import sys
 
 try:
     if len(sys.argv) != 2:
@@ -14,6 +14,27 @@ try:
         print("I'm Even.")
     else:
         print("I'm Odd.")
+
+except AssertionError as error:
+    print(f"AssertionError: {error}")
+"""
+
+import sys
+
+try:
+    assert len(sys.argv) <= 2, "more than one argument is provided"
+
+    arg = sys.argv[1]
+    assert arg.lstrip('-').isdigit(), "argument is not an integer"
+
+    number = int(arg)
+    if number % 2 == 0:
+        print("I'm Even.")
+    else:
+        print("I'm Odd.")
+
+except IndexError:
+    pass
 
 except AssertionError as error:
     print(f"AssertionError: {error}")
