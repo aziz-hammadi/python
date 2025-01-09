dd8129a24d899482abdf12ea3d5b48a6357923b1
Aziz HAMMADI <ahammad@paul-f4Ar5s6.clusters.42paris.fr>
2025-01-09 21:20:02 +0100

    gestion caractere speciaux

--- ex06/filterstring.py
+++ ex06/filterstring.py
@@ -2,33 +2,38 @@
 import sys
 from ft_filter import ft_filter
 
-
 def main():
     """
     The main function that accepts two arguments: a string S and an integer N.
-    It outputs a list of words from S that have a length greater than N.
+    It outputs a list of words from S that have a length greater than N,
+    ignoring punctuation.
 
     If the number of arguments is not two or if any argument has the wrong type,
     it raises an AssertionError.
     """
-    # Ensure correct number of arguments
-    if len(sys.argv) != 3:
-        raise AssertionError("AssertionError: the arguments are bad")
+    punctuation = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
+    try:
+        # Vérification du nombre d'arguments
+        assert len(sys.argv) == 3, "AssertionError: the arguments are bad"
 
-    # Unpack and validate arguments
-    S, N = sys.argv[1], sys.argv[2]
-    assert N.isdigit(), "AssertionError: The second argument must be an integer."
-    N = int(N)
+        # Unpack et validation des arguments
+        S, N = sys.argv[1], sys.argv[2]
+        assert N.isdigit(), "AssertionError: The second argument must be an integer."
+        N = int(N)
 
-    # Split the string into words
-    words = S.split()
+        # Suppression de la ponctuation
+        cleaned_string = S.translate(str.maketrans("", "", punctuation))
 
-    # Use lambda and ft_filter to filter words longer than N
-    filtered_words = ft_filter(lambda word: len(word) > N, words)
+        # Découpage et filtrage des mots
+        words = cleaned_string.split()
+        filtered_words = ft_filter(lambda word: len(word) > N, words)
 
-    # Output the filtered words
-    print(filtered_words)
+        # Affichage des résultats
+        print(list(filtered_words))
 
+    except AssertionError as error:
+        print(error)
+        sys.exit(1)
 
 if __name__ == "__main__":
     main()
--- ex06/ft_filter.py
+++ ex06/ft_filter.py
@@ -1,5 +1,8 @@
 def ft_filter(function, iterable):
-    ft_filter.___doc___ = ```Construct an iterator from those elements of iterable for which function is true. iterable may be either a sequence, a container which supports iteration, or an iterator. \nIf function is None, the identity function is assumed, that is, all elements of iterable that are false are removed.```
+    """filter(function or None, iterable) --> filter object
+
+Return an iterator yielding those items of iterable for which function(item)
+is true. If function is None, return the items that are true."""
     if function:
-        return (item for item in iterable if function(item))
-    return (item for item in iterable if item)
+        return [item for item in iterable if function(item)]
+    return [item for item in iterable if item]
